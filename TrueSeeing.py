80aba76d77fe449ddc225592fbeb5a3a6de923d3
ybldnpy <18508782239@163.com>
2024-04-19 17:10:25 +0800

    develop encoding, file selction, getting content from clipboard and signature algorithm

--- TrueSeeing.py
+++ TrueSeeing.py
@@ -1,9 +1,14 @@
 
 import unicodedata
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import scrolledtext
 from tkinter import filedialog
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pss
 from Crypto.Hash import SHA256
+from Crypto import Random
+import os
 
 
 class TrueSeeingApp:
@@ -19,12 +24,13 @@ class TrueSeeingApp:
 
         self.graphical_characters_unicode:list  = []
         self.harmful_format_unicode:list = []
-        self.harmless_format:list = []
+        self.harmless_format:list = ['\t', '\n', '\r']
         for code_point in range(0, 0x110000):
             char = chr(code_point)
             category = unicodedata.category(char)
-            if char.isascii() and not char.isprintable():
-                self.harmless_format.append(char)
+            if char.isascii():
+                if (not char.isprintable()) and (not char in self.harmless_format):
+                   self.harmful_format_unicode.append(char)
             elif char.isprintable():
                 self.graphical_characters_unicode.append(char)
             else:
@@ -103,6 +109,7 @@ class TrueSeeingApp:
         self.root.title("TrueSeeing")
         self.init_list()
 
+        self.encoding_format = encoding_format
 
         # p1
         self.label_p1 = tk.Label(self.root,text="original")
@@ -110,16 +117,21 @@ class TrueSeeingApp:
         self.text_area_p1 = tk.Text(self.root,height=2)
         self.text_area_p1.pack(padx=10,pady=10)
 
-        # get content from clipboard
-        if self.root.clipboard_get().strip():
-            self.text_area_p1.insert("1.0", self.root.clipboard_get())
 
         # get content from local file
-        self.button_import_local_file = tk.Button(self.root, text="import local file", command=lambda: self.import_local_file()).pack()
+        self.button_import_local_file = tk.Button(self.root, text="import local file", command=lambda: self.import_local_file(self.encoding_format)).pack()
 
         # get content from typing
         self.text_area_p1.bind("<KeyRelease>",self.show_true_text)
 
+        # paste
+        # callback function will read binary buffer from clipboard and decode using encoding format
+        # self.text_area_p1.bind("<Control-v>",self.input_from_clipboard)
+        # self.text_area_p1.bind("<Control-V>",self.input_from_clipboard)
+
+        # get content from clipboard
+        # if self.root.clipboard_get().strip():
+        #     self.text_area_p1.insert("1.0", self.root.clipboard_get())
 
         # p2
         self.label_p2 = tk.Label(self.root,text="True text")
@@ -128,20 +140,51 @@ class TrueSeeingApp:
         self.text_area_p2.pack(padx=10,pady=10)
         self.text_area_p2.config(state='disabled')
 
+
+        # generate signature
+        self.signature_button = tk.Button(self.root,text="Generate signature",command=lambda:self.generate_signature(self.text_area_p1.get("1.0",tk.END)))
+        self.signature_button.pack(padx=10,pady=10)
+        self.signature_text = tk.Text(self.root,height=3)
+        self.signature_text.pack(padx=10,pady=10)
+        self.signature_text.config(state='disabled')
+
+
+        # self.public_key = tk.StringVar(self.root,"Public key:(N = NaN,e = NaN)")
+        # self.public_key_label = tk.Label(self.root,textvariable=self.public_key)
+        # self.public_key_label.pack(padx=10,pady=10)
+
+        # show algorithm
+        self.algorithm_label = tk.Label(self.root,text="Algorithm: RSA-FDH: RSA full-domain hash")
+        self.algorithm_label.pack(padx=10,pady=10)
+        
+
     # import local file to p1
-    def import_local_file(self):
-        self.local_file_path = filedialog.askopenfilename()
-        if self.local_file_path:
-            with open(self.local_file_path, 'rb') as file:
-                self.local_file_content = file.read()
+    def import_local_file(self,encoding_format):
+        local_file_path = filedialog.askopenfilename()
+        # max file size 10MB
+        if local_file_path:
+            try:
+                with open(local_file_path, 'rb') as file:
+                    local_file_content = file.read()
                 self.text_area_p1.delete("1.0", tk.END)
-                self.text_area_p1.insert("1.0", self.local_file_content)
-
-
+                # 尝试用用户提供的编码格式解码文件内容
+                decoded_content = local_file_content.decode(encoding_format)
+                self.text_area_p1.insert("1.0", decoded_content)
+                self.show_true_text(None)
+            except UnicodeDecodeError as e:
+                # 如果解码失败，弹出一个警告框
+                messagebox.showerror("Encoding error", f"File encoding is not {encoding_format}")
+            except Exception as e:
+                # 处理其他可能的异常
+                messagebox.showerror("Error", f"Error: {str(e)}")
     # clear the panel
     def clear_panel(self):
         for widget in self.root.winfo_children():
             widget.destroy()
+
+    # def input_from_clipboard(self,e):
+        
+    #     return "break"
 
     def check_text(self):
         text = self.text_area_p1.get("1.0", tk.END).strip()
@@ -160,8 +203,8 @@ class TrueSeeingApp:
 
     def detect_harmful_characters(self, text):
         # Example: Detecting harmful characters (e.g., non-ASCII characters)
-        harmful_characters = [char for char in text if ord(char) > 127]
-        return harmful_characters
+        harmful_characters = [char for char in text if char in self.graphical_characters_unicode or char in self.harmful_format_unicode]
+        return len(harmful_characters) > 0
 
     def display_warning(self, message):
         warning_window = tk.Toplevel(self.root)
@@ -173,17 +216,63 @@ class TrueSeeingApp:
         ok_button = tk.Button(warning_window, text="OK", command=warning_window.destroy)
         ok_button.pack(pady=5)
 
+
+    # create public and private key pair
+    def generate_keys(self,bits):
+        private_key_path = "private.pem"
+        public_key_path = "public.pem"
+        private_key = None
+        public_key = None
+        # 检查私钥和公钥文件是否都存在
+        if os.path.exists(private_key_path) and os.path.exists(public_key_path):
+            # 导入现有密钥
+            with open(private_key_path, 'rb') as priv_file:
+                private_key = RSA.import_key(priv_file.read())
+            with open(public_key_path, 'rb') as pub_file:
+                public_key = RSA.import_key(pub_file.read())
+        else:
+            # 生成新的密钥对
+            key = RSA.generate(bits, Random.new().read)
+            private_key = key.export_key()
+            public_key = key.publickey().export_key()
+            
+            # 保存私钥和公钥到文件
+            with open(private_key_path, 'wb') as priv_file:
+                priv_file.write(private_key)
+            with open(public_key_path, 'wb') as pub_file:
+                pub_file.write(public_key)
+
+            # 重新导入密钥，确保一致性
+            private_key = RSA.import_key(private_key)
+            public_key = RSA.import_key(public_key)
+
+        return private_key, public_key
+    def sign_text(self,text:str):
+        key_bits = 2048
+        private_key, public_key = self.generate_keys(key_bits)
+        # private_key = RSA.import_key(private_key_bytes)
+        # public_key = RSA.import_key(public_key_bytes)
+        modulus = private_key.n
+        text_buffer = text.encode(encoding=self.encoding_format)
+        hash_obj = SHA256.new(text_buffer)
+        hashed_integer = int.from_bytes(hash_obj.digest(), byteorder='big')
+        signature = pow(hashed_integer, private_key.d, private_key.n)
+        self.algorithm_label
+        return signature.to_bytes(key_bits//8,byteorder="big")
+    # only runs until all characters in text belong to category 1 or 3
     def generate_signature(self, text):
-        # Example: Generating a digital signature using SHA-256
-        hasher = SHA256.new()
-        hasher.update(text.encode('utf-8'))
-        signature = hasher.hexdigest()
-        return signature
-    
+        if self.detect_harmful_characters(text):
+            self.display_warning("Harmful characters exist")
+            return
+        signature= self.sign_text(text)
+        self.signature_text.config(state="normal")
+        self.signature_text.delete("1.0",tk.END)
+        self.signature_text.insert("end",signature.hex())
+        self.signature_text.config(state="disabled")
+        
     def run(self):
         self.root.mainloop()
         
-
 
 # Create the main window
 
