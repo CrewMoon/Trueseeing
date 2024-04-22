2970d7f7c90efadfd6efc1ff8eafb515abf00cb8
CrewMoon <kemu.xu20@student.xjtlu.edu.cn>
2024-04-22 15:56:42 +0800

    update some GUI design but lack some exception handling

--- TrueSeeing.py
+++ TrueSeeing.py
@@ -20,11 +20,11 @@ class TrueSeeingApp:
         self.graphical_characters_ascii.extend(chr(i) for i in range(65, 91))
         self.graphical_characters_ascii.extend(chr(i) for i in range(97, 123))
         self.graphical_characters_ascii.extend(str(i) for i in range(0,10))
-        self.graphical_characters_ascii.extend("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~""")
+        self.graphical_characters_ascii.extend("""!"#$%&"()*+,-./:;<=>?@[\]^_`{|}~""")
 
         self.graphical_characters_unicode:list  = []
         self.harmful_format_unicode:list = []
-        self.harmless_format:list = ['\t', '\n', '\r']
+        self.harmless_format:list = ["\t", "\n", "\r"]
         for code_point in range(0, 0x110000):
             char = chr(code_point)
             category = unicodedata.category(char)
@@ -59,7 +59,7 @@ class TrueSeeingApp:
 
     def show_true_text(self,event):
         originalText:str = self.text_area_p1.get("1.0","end")
-        self.text_area_p2.configure(state='normal')
+        self.text_area_p2.configure(state="normal")
         self.text_area_p2.delete("1.0","end")
         originalText = originalText[0:len(originalText)-1]
         self.is_benign = False
@@ -84,7 +84,7 @@ class TrueSeeingApp:
                 self.text_area_p2.tag_add("color3","end-1c")
                 self.text_area_p2.tag_config("color3",foreground="green")
                 self.is_benign = True
-        self.text_area_p2.configure(state='disabled')
+        self.text_area_p2.configure(state="disabled")
         if (self.is_benign):
             self.signature_button.config(state="disabled")
         else:
@@ -94,7 +94,7 @@ class TrueSeeingApp:
     def GUI_init(self):
         self.root.title("TrueSeeing")
         self.root.iconbitmap("TrueSeeingIcon.ico")
-        self.root.geometry("500x500+200+200")
+        self.root.geometry("500x800+200+200")
         self.root.resizable(False, False)
 
         self.frame_panel_labels = tk.Frame(self.root)
@@ -110,12 +110,13 @@ class TrueSeeingApp:
 
     # choose encode format panel
     def GUI_choose_encoding_format(self):
-        # init the panel
+        # init the panel head
         self.label_encoding_format.config(relief="sunken")
 
         self.encoding_format = tk.StringVar()
         self.encoding_format.set("UTF-8")
 
+        # panel body
         self.frame_CEF = tk.Frame(self.root)
         self.frame_CEF.pack()
         self.label = tk.Label(self.frame_CEF, text="Select Encoding:")
@@ -127,10 +128,18 @@ class TrueSeeingApp:
         self.button_choose_encoding_format = tk.Button(self.frame_CEF, cursor="hand2", text="OK", command=lambda: self.GUI_text_examination(self.encoding_format.get()))
         self.button_choose_encoding_format.pack()
 
+        # panel bottom
+        self.label_frame_SCF = tk.LabelFrame(self.frame_CEF, text="tip")
+        self.label_frame_SCF.pack(fill="both", padx=4)
+        self.label_tip_CEF = tk.Label(self.label_frame_SCF, text="You have to choose the encoding format from UTF-8 and UTF-16.\n")
+        self.label_tip_CEF.pack()
+
+
+
 
     # text examination panel
     def GUI_text_examination(self, encoding_format):
-        # init the panel
+        # init the panel head
         self.frame_CEF.pack_forget()
         self.label_encoding_format.config(relief="raised")
         self.label_text_examination.config(relief="sunken")
@@ -138,20 +147,35 @@ class TrueSeeingApp:
 
         self.encoding_format = encoding_format
 
+        # panel body
         self.frame_TE = tk.Frame(self.root)
         self.frame_TE.pack()
+
         # p1
         self.label_p1 = tk.Label(self.frame_TE,text="original")
         self.label_p1.pack(padx=10, pady=10)
-        self.text_area_p1 = tk.Text(self.frame_TE,height=2)
-        self.text_area_p1.pack(padx=10, pady=10)
 
+        #p1 text area frame
+        self.frame_text_area_p1 = tk.Frame(self.frame_TE)
+        self.frame_text_area_p1.pack()
+
+        self.scrollbar_p1 = tk.Scrollbar(self.frame_text_area_p1)
+        self.scrollbar_p1.pack(side="right", fill="y")
+
+        self.text_area_p1 = tk.Text(self.frame_text_area_p1,height=10, yscrollcommand=self.scrollbar_p1.set)
+        self.text_area_p1.pack()
+
+        self.scrollbar_p1.config(command=self.text_area_p1.yview)
+
+        # get content from clipboard
+        self.button_clipboard = tk.Button(self.frame_TE, text="paste", command=lambda: self.input_from_clipboard())
+        self.button_clipboard.pack(padx=10, pady=10)
 
         # get content from local file
         self.button_import_local_file = tk.Button(self.frame_TE, text="import local file", command=lambda: self.import_local_file(encoding_format))
         self.button_import_local_file.pack(padx=10, pady=10)
 
-        # get content from typing
+        # get content from typing, update p2 as long as enter any content in p1
         self.text_area_p1.bind("<KeyRelease>",self.show_true_text)
 
         # paste
@@ -163,13 +187,49 @@ class TrueSeeingApp:
         # if self.root.clipboard_get().strip():
         #     self.text_area_p1.insert("1.0", self.root.clipboard_get())
 
+        # p1 tip
+        self.label_frame_TE_p1 = tk.LabelFrame(self.frame_TE, text="tip")
+        self.label_frame_TE_p1.pack(fill="both", padx=4)
+        self.label_tip_p1 = tk.Label(self.label_frame_TE_p1,
+                                          text="To examine the text, \n"
+                                               "you can copy an external text to the above text area \n"
+                                               "or import a local file, and edit on the text.\n"
+                                               "And we show the original appearance of the input in above text area.\n")
+        self.label_tip_p1.pack()
+
         # p2
         self.label_p2 = tk.Label(self.frame_TE,text="True text")
         self.label_p2.pack(padx=10, pady=10)
-        self.text_area_p2 = tk.Text(self.frame_TE,height=2)
-        self.text_area_p2.pack(padx=10, pady=10)
-        self.text_area_p2.config(state='disabled')
+        # p2 text area frame
+        self.frame_text_area_p2 = tk.Frame(self.frame_TE)
+        self.frame_text_area_p2.pack()
 
+        self.scrollbar_p2 = tk.Scrollbar(self.frame_text_area_p2)
+        self.scrollbar_p2.pack(side="right", fill="y")
+
+        self.text_area_p2 = tk.Text(self.frame_text_area_p2,height=10, yscrollcommand=self.scrollbar_p2.set)
+        self.text_area_p2.pack()
+
+        self.scrollbar_p2.config(command=self.text_area_p2.yview)
+
+        self.text_area_p2.config(state="disabled")
+
+        # p2 tip
+        self.label_frame_TE_p2 = tk.LabelFrame(self.frame_TE, text="tip")
+        self.label_frame_TE_p2.pack(fill="both", padx=4)
+        self.label_tip_p2 = tk.Label(self.label_frame_TE_p2,
+                                          text="As long as you input, we check the content instantly.\n"
+                                               "In the above text area, \n"
+                                               "the characters are divided into four categories and shown in different colors:\n"
+                                               "1. English graphic characters: in black\n"
+                                               "2. All other Unicode graphic characters: in blue\n"
+                                               "3. Harmless Unicode format characters: in red\n"
+                                               "4. All other Unicode format characters that are potentially harmful: in green\n"
+                                               "As long as the character category 2 and 4 exist, the signature button stays disabled.\n")
+        self.label_tip_p2.pack()
+
+
+        # signature button
         self.signature_button = tk.Button(self.frame_TE,text="Generate signature",command=lambda:self.GUI_signature())
         self.signature_button.pack(padx=10, pady=10)
 
@@ -187,7 +247,7 @@ class TrueSeeingApp:
         # generate signature
         self.signature_text = tk.Text(self.frame_S,height=3)
         self.signature_text.pack(padx=10,pady=10)
-        self.signature_text.config(state='disabled')
+        self.signature_text.config(state="disabled")
 
         self.generate_signature(self.text_area_p1.get("1.0", tk.END))
 
@@ -197,8 +257,17 @@ class TrueSeeingApp:
         # self.public_key_label.pack(padx=10,pady=10)
 
         # show algorithm
-        self.algorithm_label = tk.Label(self.frame_S,text="Algorithm: RSA-FDH: RSA full-domain hash")
+        self.algorithm_label = tk.Label(self.frame_S,text="We have successfully generate the signature for this text\n"
+                                                          "in the above text area with the algorithm:\n"
+                                                          "RSA-FDH: RSA full-domain hash.\n")
         self.algorithm_label.pack(padx=10,pady=10)
+
+        # algorithm tip
+        self.label_frame_S_algorithm = tk.LabelFrame(self.frame_S, text="tip")
+        self.label_frame_S_algorithm.pack(fill="both", padx=4)
+        self.label_tip_algorithm = tk.Label(self.label_frame_S_algorithm,
+                                          text="You can find the file named \"private\" for private key and \"public\" for public key.\n")
+        self.label_tip_algorithm.pack()
 
     # import local file to p1
     def import_local_file(self,encoding_format):
@@ -206,7 +275,7 @@ class TrueSeeingApp:
         # max file size 10MB
         if local_file_path:
             try:
-                with open(local_file_path, 'rb') as file:
+                with open(local_file_path, "rb") as file:
                     local_file_content = file.read()
                 self.text_area_p1.delete("1.0", tk.END)
                 # 尝试用用户提供的编码格式解码文件内容
@@ -224,9 +293,9 @@ class TrueSeeingApp:
         for widget in self.root.winfo_children():
             widget.destroy()
 
-    # def input_from_clipboard(self,e):
-        
-    #     return "break"
+    def input_from_clipboard(self):
+        self.text_area_p1.insert("1.0", self.root.clipboard_get())
+        self.show_true_text(None)
 
     def check_text(self):
         text = self.text_area_p1.get("1.0", tk.END).strip()
@@ -268,9 +337,9 @@ class TrueSeeingApp:
         # 检查私钥和公钥文件是否都存在
         if os.path.exists(private_key_path) and os.path.exists(public_key_path):
             # 导入现有密钥
-            with open(private_key_path, 'rb') as priv_file:
+            with open(private_key_path, "rb") as priv_file:
                 private_key = RSA.import_key(priv_file.read())
-            with open(public_key_path, 'rb') as pub_file:
+            with open(public_key_path, "rb") as pub_file:
                 public_key = RSA.import_key(pub_file.read())
         else:
             # 生成新的密钥对
@@ -279,9 +348,9 @@ class TrueSeeingApp:
             public_key = key.publickey().export_key()
             
             # 保存私钥和公钥到文件
-            with open(private_key_path, 'wb') as priv_file:
+            with open(private_key_path, "wb") as priv_file:
                 priv_file.write(private_key)
-            with open(public_key_path, 'wb') as pub_file:
+            with open(public_key_path, "wb") as pub_file:
                 pub_file.write(public_key)
 
             # 重新导入密钥，确保一致性
@@ -298,7 +367,7 @@ class TrueSeeingApp:
         modulus = private_key.n
         text_buffer = text.encode(encoding=self.encoding_format)
         hash_obj = SHA256.new(text_buffer)
-        hashed_integer = int.from_bytes(hash_obj.digest(), byteorder='big')
+        hashed_integer = int.from_bytes(hash_obj.digest(), byteorder="big")
         signature = pow(hashed_integer, private_key.d, private_key.n)
         return signature.to_bytes(key_bits//8,byteorder="big")
 
